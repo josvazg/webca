d18f24b4464015205020e99575fcdcd216d21deb
Jose Luis Vázquez González <josvazg@gmail.com>
2012-06-07 14:19:48 +0200

    fixing and improving navigation, still needs some cleanup and simplification

--- ui.go
+++ ui.go
@@ -7,24 +7,33 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
+	//"os"
 	"strconv"
 	"strings"
 )
 
+const (
+	SETUPADDR    = "127.0.0.1:80"
+	ALTSETUPADDR = "127.0.0.1:9090"
+	PAGESTATUS   = "pageStatus"
+	_HTML        = ".html"
+)
+
+// store for web sessions
 var store = sessions.NewCookieStore([]byte("34534askjdfhkjsd41234rrf34856"))
 
+// templates contains all web templates
 var templates = template.Must(template.ParseFiles("html/setup.html", "html/newuser.html",
 	"html/newca.html", "html/newcert.html",
 	"html/templates.html", "html/style.css", "html/translate_en.html"))
 
-var defaultHandler func (w http.ResponseWriter, r *http.Request)
+// templateIndex contains a quick way to test page template existence
+var templateIndex map[string]*template.Template
 
-const (
-	SETUPADDR    = "127.0.0.1:80"
-	ALTSETUPADDR = "127.0.0.1:9090"
-)
+// defaultHandler points to the handler for '/' requests
+var defaultHandler func(w http.ResponseWriter, r *http.Request)
 
+// SetupWizard contains the status of the setup wizard
 type SetupWizard struct {
 	Step   int
 	U      *User
@@ -33,75 +42,77 @@ type SetupWizard struct {
 	Port   string
 }
 
+// PageStatus contains all values that a page and its templates need (including the SetupWizard)
 type PageStatus struct {
 	SetupWizard
 	Error string
 }
 
+// Tr allows to access tr from templates as well
 func (ps *PageStatus) Tr(s string) string {
 	return tr(s)
 }
 
-// Transaltion function
+// tr is the app translation function
 func tr(s string) string {
 	return s
 }
 
 // webca starts the setup if there is no HTTPS config or the normal app if it is present
 func webca() {
+	// build templateIndex
+	templateIndex = make(map[string]*template.Template)
+	for _, t := range templates.Templates() {
+		if strings.HasSuffix(t.Name(), _HTML) {
+			templateIndex[t.Name()] = t
+		}
+	}
+	// load config to run the normal app or the setup wizard
 	cfg := LoadConfig()
 	if cfg == nil {
 		setup()
 	}
 }
 
+// startSetup starts the setup wizard web page sequence
 func startSetup(w http.ResponseWriter, r *http.Request) {
-	ps := &PageStatus{SetupWizard: SetupWizard{Step: 1}}
-	context.DefaultContext.Set(r, "pageStatus", ps)
-	r.URL,_=r.URL.Parse("/newuser")
-	autoPage(w, r)
+	ps := &PageStatus{SetupWizard: SetupWizard{Step: 1, U: &User{}}}
+	forwardTo(w, r, ps, "newuser")
 }
 
+// userSetup saves a new user
 func userSetup(w http.ResponseWriter, r *http.Request) {
-	ps := &PageStatus{SetupWizard: SetupWizard{Step: 1, U:&User{}}}
+	ps := &PageStatus{SetupWizard: SetupWizard{Step: 1, U: &User{}}}
 	ps.Step, _ = strconv.Atoi(r.FormValue("Step"))
 	ps.U.Username = r.FormValue("Username")
 	ps.U.Fullname = r.FormValue("Fullname")
 	ps.U.Email = r.FormValue("Email")
 	pwd := r.FormValue("Password")
 	pwd2 := r.FormValue("Password2")
-	if pwd == "" || pwd != pwd2 {
-		ps.Error = tr("BadPasswd")
-		context.DefaultContext.Set(r, "pageStatus", ps)
-		r.URL,_=r.URL.Parse("/newuser")
-		autoPage(w, r)
+	if pwd == "" {
+		ps.Error = tr("Password is empty!")
+		forwardTo(w, r, ps, "newuser")
+		return
+	} else if pwd != pwd2 {
+		ps.Error = tr("Passwords don't match!")
+		forwardTo(w, r, ps, "newuser")
 		return
 	}
 	ps.U.Password = pwd
 	ps.Step += 1
 	log.Println(ps.U)
 	session, _ := store.Get(r, "")
-	_,ok:=session.Values["ps"]
+	_, ok := session.Values[PAGESTATUS]
 	if ok {
-		session.Values["ps"].(*PageStatus).U=ps.U
+		session.Values[PAGESTATUS].(*PageStatus).U = ps.U
 	} else {
-		session.Values["ps"] = ps
+		session.Values[PAGESTATUS] = ps
 	}
-	newsetup(w, r)
+	forwardTo(w, r, nil, "newca")
 }
 
-func newsetup(w http.ResponseWriter, r *http.Request) {
-	ps := context.DefaultContext.Get(r, "pageStatus").(*PageStatus)
-	if ps.M.User == "" {
-		ps.M.User = ps.U.Email
-	}
-	err := templates.ExecuteTemplate(w, "setup.html", ps)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func saveSetup(w http.ResponseWriter, r *http.Request) {
+// mailerSetup configures the mailer settings
+func mailerSetup(w http.ResponseWriter, r *http.Request) {
 	ps := &PageStatus{}
 	ps.Step, _ = strconv.Atoi(r.FormValue("Step"))
 	ps.M.User = r.FormValue("Email")
@@ -111,8 +122,7 @@ func saveSetup(w http.ResponseWriter, r *http.Request) {
 	pwd2 := r.FormValue("Password2")
 	if pwd == "" || pwd != pwd2 {
 		ps.Error = tr("BadPasswd")
-		context.DefaultContext.Set(r, "pageStatus", ps)
-		newsetup(w, r)
+		forwardTo(w, r, ps, "newsetup")
 		return
 	}
 	ps.M.Server = ps.Server
@@ -121,78 +131,92 @@ func saveSetup(w http.ResponseWriter, r *http.Request) {
 	}
 	ps.M.Passwd = pwd
 	log.Println(ps.M)
-	context.DefaultContext.Set(r, "pageStatus", ps)
+	context.DefaultContext.Set(r, PAGESTATUS, ps)
 	session, _ := store.Get(r, "")
-	session.Values["mailer"] = ps.M
-	r.URL,_=r.URL.Parse("/newca")
+	_, ok := session.Values[PAGESTATUS]
+	if ok {
+		session.Values[PAGESTATUS].(*PageStatus).M = ps.M
+	} else {
+		session.Values[PAGESTATUS] = ps
+	}
+	// TODO finish setup and start the webca
+}
+
+// forwardTo passes control to the given page
+func forwardTo(w http.ResponseWriter, r *http.Request, ps *PageStatus, page string) {
+	if ps != nil {
+		context.DefaultContext.Set(r, PAGESTATUS, ps)
+	}
+	r.URL, _ = r.URL.Parse("/" + page)
 	autoPage(w, r)
 }
 
+// autoPage loads current pageStatus and then displays the page given in the URL
 func autoPage(w http.ResponseWriter, r *http.Request) {
-	ps:=getPageStatus(r)
-	page:=page(r)
-	if page=="" {
-		defaultHandler(w,r)
+	ps := getPageStatus(r)
+	page := page(r)
+	if page == "" {
+		defaultHandler(w, r)
 		return
 	}
 	if !checkPage(page) {
 		http.NotFound(w, r)
 		return
 	}
-	log.Println("uri=",r.URL.RequestURI(),"page=",page)
-	err := templates.ExecuteTemplate(w, page+".html", ps)
+	log.Println("uri=", r.URL.RequestURI(), "page=", page)
+	err := templates.ExecuteTemplate(w, page+_HTML, ps)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+// checkPage tells whether the given page does really exist or not
 func checkPage(page string) bool {
-	if page=="templates" {
+	if page == "templates" {
 		return false
 	}
-	_, err := os.Stat("html/"+page+".html")
-	if os.IsNotExist(err) {
-		return false
-	}
-	return true
-
+	_, ok := templateIndex[page+_HTML]
+	return ok
 }
 
+// page extracts the page the user whats to go to from the URL
 func page(r *http.Request) string {
-	pg:=r.URL.RequestURI()
+	pg := r.URL.RequestURI()
 	if strings.Contains(pg, "?") {
-		pg=strings.Split(pg, "?")[0]
+		pg = strings.Split(pg, "?")[0]
 	}
 	if strings.HasPrefix(pg, "/") {
-		pg=pg[1:]
+		pg = pg[1:]
 	}
 	if strings.Contains(pg, "/") {
-		parts:=strings.Split(pg, "/")
-		pg=parts[len(parts)-1]
+		parts := strings.Split(pg, "/")
+		pg = parts[len(parts)-1]
 	}
 	if strings.Contains(pg, ".") {
-		pg=strings.Split(pg,".")[0]
+		pg = strings.Split(pg, ".")[0]
 	}
 	return pg
 }
 
+// getPageStatus loads current pageStatus (from request or session)
 func getPageStatus(r *http.Request) *PageStatus {
-	ips:=context.DefaultContext.Get(r, "pageStatus")
-	if ips==nil {
-	    session, _ := store.Get(r, "webca")
-	    ips=session.Values["pageStatus"]
+	ips := context.DefaultContext.Get(r, PAGESTATUS)
+	if ips == nil {
+		session, _ := store.Get(r, "webca")
+		ips = session.Values[PAGESTATUS]
 	}
-	if ips!=nil {
+	if ips != nil {
 		return ips.(*PageStatus)
 	}
 	return &PageStatus{}
 }
 
+// setup starts the webca "setup wizard"
 func setup() {
 	log.Printf("(Warning) Starting setup, go to http://127.0.0.1/...")
 	smux := http.NewServeMux()
 	setupServer := http.Server{Addr: SETUPADDR, Handler: smux}
-	defaultHandler=startSetup
+	defaultHandler = startSetup
 	smux.HandleFunc("/", autoPage)
 	smux.HandleFunc("/userSetup", userSetup)
 	err := setupServer.ListenAndServe()
